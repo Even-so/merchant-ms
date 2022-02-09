0b75f7d260231f08bc9b68466d4bbeb4cb83ae73
xiaorikui <15219285293@163.com>
2022-02-09 15:50:53 +0800

    项目开发

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+//引入elementUI
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI, { size: "small", zIndex: 3000 });
 
-Vue.config.productionTip = false
+//引入axios
+import axios from "axios";
+
+//引入Jsonp，解决跨域
+import { VueJsonp } from "vue-jsonp";
+Vue.use(VueJsonp);
+
+//配置全局根路径
+//外网服务器地址
+axios.defaults.baseURL = "http://120.76.52.65/phpsaomaserver";
+//配置图片访问地址常量
+Vue.prototype.imgUrl = "http://120.76.52.65/phpsaomaserver/uploads/";
+Vue.prototype.baseURL = "http://120.76.52.65/phpsaomaserver";
+// Vue.prototype.baseURL = "http://localhost/phpsaomaserver";
+// axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+//将axios挂载到vue原型中
+// axios.defaults.headers.common['Device'] = 111111;
+// axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
+
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
